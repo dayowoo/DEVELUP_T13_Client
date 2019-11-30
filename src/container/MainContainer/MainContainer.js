2a59beb9f732b013e65e128e35612b5b3fb28d4a
GwonHeeJun <gwonhj9751@naver.com>
2019-11-30 21:41:37 +0900

    [UPDATE] Connect Component for Container

--- src/container/MainContainer/MainContainer.js
+++ src/container/MainContainer/MainContainer.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
 import Header from '../../components/Header/Header'
+import Login from '../../components/Login/Login'
+import SubHeader from '../../components/SubHeader/SubHeader'
+import LastedList from '../../components/LastedList/LastedList'
+import Event from '../../components/Event/Event'
 
 class MainContainer extends Component {
     render() {
         return (
            <React.Fragment>
                <Header />
+               <SubHeader />
+               <LastedList />
+               <Event />
+               <LastedList />
+               <LastedList />
            </React.Fragment>
         )
     }
